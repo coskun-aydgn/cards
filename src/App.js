d89c6d5dcefc96f13d7f4729afb0ab387ebead1c
coskun.aydgn <coskun.aydgn@gmail.com>
2024-05-23 21:11:38 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import 'bulma/css/bulma.css';
 import Course  from './Course';
 import Anguler from './images/angular.jpg';
 import Bootsrap5 from './images/bootstrap5.png';
@@ -10,35 +11,51 @@ import Kompleweb from './images/kompleweb.jpg';
 function App() {
   return (
     <div className="App">
-      {/* <img src={Anguler} alt=''/> */}
-      <Course 
-      image={Anguler}
-      title='JS' 
-      description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus 
-      ipsum, a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">My Skill</p>
+        </div>
+      </section>
+      <div className='container'>
+        <section className='section'>
+        <div className='columns'>
+            <div className='column'>
+              <Course 
+              image={Anguler}
+              title='JS' 
+              description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus 
+              ipsum, a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+            </div>
+            <div className='column'>
+                <Course 
+                image={Bootsrap5}
+                title='Bootstrap 5' 
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
+                a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+            </div>
+            <div className='column'>
+                <Course 
+                 image={Ccsharp}
+                  title='Frontend' 
+                  description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
+                  a reiciendis error eaque aliquam. Praesentium, veniam!'/>
 
-      <Course 
-         image={Bootsrap5}
-      title='Bootstrap 5' 
-      description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
-       Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
-       a reiciendis error eaque aliquam. Praesentium, veniam!'/>
-
-      <Course 
-         image={Ccsharp}
-        title='Frontend' 
-        description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
-        a reiciendis error eaque aliquam. Praesentium, veniam!'/>
-
-      <Course
-         image={Kompleweb}
-       title='SQL' 
-       description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
-      a reiciendis error eaque aliquam. Praesentium, veniam!'/>
-      
+            </div>
+            <div className='column'>
+                <Course
+                image={Kompleweb}
+                title='SQL' 
+                description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
+                a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+            </div>
+        </div>
+        </section>
+        
+      </div>
     </div>
   );
 }
