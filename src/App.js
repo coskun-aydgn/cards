1960ffc778af730b1f4c661e9f2ceb9e81ba363d
coskun.aydgn <coskun.aydgn@gmail.com>
2024-05-23 19:49:52 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Course  from './Course';
+import Anguler from './images/angular.jpg';
+import Bootsrap5 from './images/bootstrap5.png';
+import Ccsharp from './images/ccsharp.png';
+import Kompleweb from './images/kompleweb.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={Anguler} alt=''/> */}
+      <Course 
+      image={Anguler}
+      title='JS' 
+      description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus 
+      ipsum, a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+
+      <Course 
+         image={Bootsrap5}
+      title='Bootstrap 5' 
+      description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
+       a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+
+      <Course 
+         image={Ccsharp}
+        title='Frontend' 
+        description='Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
+        a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+
+      <Course
+         image={Kompleweb}
+       title='SQL' 
+       description='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Harum autem consequatur ipsa maiores cumque ab libero expedita dignissimos culpa cum accusamus ipsum, 
+      a reiciendis error eaque aliquam. Praesentium, veniam!'/>
+      
     </div>
   );
 }
